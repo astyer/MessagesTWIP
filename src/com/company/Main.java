955e891c7be508446c3428d5e8d8a9005307ea9a
astyer <88atti@gmail.com>
2017-02-02 10:34:01 -0500

    in progress

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-        long cTime = System.currentTimeMillis();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to messenger!");
+
+        boolean go = true;
+        while(go)
+        {
+            System.out.println("Please enter your phone number");
+            String pn = input.nextLine();
+            System.out.println("Please enter your message");
+            String msg = input.nextLine();
+            long cTime = System.currentTimeMillis();
+            Message m = new Message(pn, msg, cTime);
+            messageList msgs = new messageList();
+
+            msgs.add(m);
+
+            System.out.println("Would you like to continue messaging? (yes or no)");
+            String yn = input.nextLine();
+            if(yn.equalsIgnoreCase("no"))
+            {
+                go = false;
+            }
+        }
 
     }
 }
--- src/com/company/messageList.java
+++ src/com/company/messageList.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.*;
+
 public class messageList {
 
+    public ArrayList a = new ArrayList();
+
     public void add(Message m)
+    {
+        a.add(m);
+    }
+
+    public void move(int position, Message m)
     {
 
     }
